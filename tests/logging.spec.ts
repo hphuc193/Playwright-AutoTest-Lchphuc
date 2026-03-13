0c9f3bb668cda5842e7061403acf82f0f7e7fb1d
hphuc193 <phuc190324@gmail.com>
2026-03-13 09:37:34 +0700

    feat: Testcase Loggin with DDT

--- tests/logging.spec.ts
+++ tests/logging.spec.ts
@@ -1,16 +1,69 @@
 import { test, expect } from '@playwright/test';
+import loginData from '../test-data/loginData.json';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/');
+test.describe('OrangeHRM Login - Data Driven', () => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/OrangeHRM/);
+  loginData.forEach((data) => {
 
-  await page.fill('input[name="username"]', 'Admin');
-  await page.fill('input[name="password"]', 'admin123');
-  await page.click('button[type="submit"]');
+    test(data.name, async ({ page }) => {
 
-  await expect(page).toHaveURL(/dashboard/);
+      // mở trang login
+      await page.goto(
+        'https://opensource-demo.orangehrmlive.com/',
+        { waitUntil: 'domcontentloaded' }
+      );
 
-await page.click('a[href*="recruitment"]');
+      // nhập username
+      await page.locator('input[name="username"]').fill(data.input.username);
+
+      // nhập password
+      await page.locator('input[name="password"]').fill(data.input.password);
+
+      // click login
+      await page.getByRole('button', { name: 'Login' }).click();
+
+      // SUCCESS CASE
+      if (data.expect.result === 'success') {
+
+        await page.waitForURL('**/dashboard/**', { timeout: 10000 });
+        await expect(page).toHaveURL(/dashboard/);
+
+        if (data.expect.visibleSelector) {
+            await expect(
+                page.locator(data.expect.visibleSelector)
+            ).toBeVisible();
+        }
+
+      }
+
+      //VALIDATION CASE
+      else if (data.expect.result === 'validation') {
+
+        const errors = page.locator('.oxd-input-field-error-message');
+
+        if (data.expect.errorCount !== undefined) {
+            await expect(errors).toHaveCount(data.expect.errorCount);
+        }
+
+      }
+
+      //ERROR CASE
+      else if (data.expect.result === 'error') {
+
+        if (data.expect.message) {
+            const errorAlert = page.locator('.oxd-alert-content-text');
+            if (await errorAlert.count() > 0) {
+                await expect(errorAlert).toContainText(data.expect.message);
+            } 
+            else {
+                await expect(page).toHaveURL(/auth\/login/);
+            }
+        }
+
+      }
+
+    });
+
+  });
+
 });
